5f132d6cbdd770003ed898a613cb42f4522ecb23
Catalin <mandru.catalin03@gmail.com>
2024-05-09 14:02:31 +0300

    Basic fe structure

--- fe/src/App.js
+++ fe/src/App.js
@@ -1,5 +1,59 @@
 import './App.css';
+import {useState} from "react";
+import {
+    DIRTY_READ_EXPLANATION,
+    DIRTY_WRITE_EXPLANATION,
+    LOST_UPDATE_EXPLANATION,
+    PHANTOM_READ_EXPLANATION, UNREPEATABLE_READ_EXPLANATION
+} from "./explanations";
 
 export default function App() {
-    return <h1>hello</h1>;
+    const [outputText, setOutputText] = useState("");
+    const [explanationText, setExplanationText] = useState("");
+
+    const handleDirtyRead = () => {
+        setExplanationText(DIRTY_READ_EXPLANATION);
+        setOutputText("1");
+    }
+
+    const handleDirtyWrite = () => {
+        setExplanationText(DIRTY_WRITE_EXPLANATION);
+        setOutputText("2");
+    }
+
+    const handleLostUpdate = () => {
+        setExplanationText(LOST_UPDATE_EXPLANATION);
+        setOutputText("3");
+    }
+
+    const handlePhantomRead = () => {
+        setExplanationText(PHANTOM_READ_EXPLANATION);
+        setOutputText("4");
+    }
+
+    const handleUnrepeatableRead = () => {
+        setExplanationText(UNREPEATABLE_READ_EXPLANATION);
+        setOutputText("5");
+    }
+
+    return (
+        <>
+            <nav className="navbar">
+                <h1>Concurrency issues</h1>
+            </nav>
+            <div className="container">
+                <div className="button-container">
+                    <button onClick={handleDirtyRead}>Dirty Read</button>
+                    <button onClick={handleDirtyWrite}>Dirty Write</button>
+                    <button onClick={handleLostUpdate}>Lost Update</button>
+                    <button onClick={handlePhantomRead}>Phantom Read</button>
+                    <button onClick={handleUnrepeatableRead}>Unrepeatable Read</button>
+                </div>
+                <div className="output-container">
+                    <textarea className="textarea explanation" readOnly value={explanationText}/>
+                    <textarea className="textarea  output" readOnly value={outputText}/>
+                </div>
+            </div>
+        </>
+    );
 }
---
+++ fe/src/explanations.js
@@ -0,0 +1,13 @@
+const DIRTY_WRITE_EXPLANATION = "temp - dirty write explanation";
+const DIRTY_READ_EXPLANATION = "temp - dirty read explanation";
+const PHANTOM_READ_EXPLANATION = "temp - phantom read explanation";
+const UNREPEATABLE_READ_EXPLANATION = "temp - unrepeatable read explanation";
+const LOST_UPDATE_EXPLANATION = "temp - lost update explanation";
+
+export {
+    DIRTY_WRITE_EXPLANATION,
+    DIRTY_READ_EXPLANATION,
+    PHANTOM_READ_EXPLANATION,
+    UNREPEATABLE_READ_EXPLANATION,
+    LOST_UPDATE_EXPLANATION
+};
